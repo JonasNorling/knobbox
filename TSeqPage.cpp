fbb06e9427b701562c3d772755bc3eab031cf5c1
Jonas Norling <jonas.norling@gmail.com>
2012-05-17 18:50:14 +0000

    Seqencer GUI mockup

--- TSeqPage.cpp
+++ TSeqPage.cpp
@@ -6,7 +6,23 @@
 void TSeqPage::Render(uint8_t n, TDisplay::TPageBuffer* line)
 {
   if (n == 1) {
-    
+    line->DrawText("\020S1\021S2 S3 S4 all", 0);
+    line->Invert(4*TDisplay::GlyphWidth, line->GetLength());
+  }
+  else if (n == 2) {
+    line->DrawText("mode: 12 linear", LeftMargin);
+  }
+  else if (n == 3) {
+    line->DrawText("<trigger magic>", LeftMargin);
+  }
+  else if (n == 4) {
+    line->DrawText("step 3  note Eb3", LeftMargin);
+  }
+  else if (n == 5) {
+    line->DrawText("velo 097 len \035", LeftMargin);
+  }
+  else if (n == 6) {
+    line->DrawText("offs +0   CC 034", LeftMargin);
   }
   else if (n == 7) {
     char text[20];
