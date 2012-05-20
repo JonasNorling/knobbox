62cc6b63a9896a6d3869e4f5cf021ce384abeb97
Jonas Norling <jonas.norling@gmail.com>
2012-05-20 19:18:43 +0000

    Remove dependency on lambda, because my Debian machine is too conservative.

--- tests/TestMidi.cpp
+++ tests/TestMidi.cpp
@@ -1,13 +1,16 @@
 #include "TMidi.h"
 #include <vector>
 
+static std::vector<uint8_t> data;
+void push_data(uint32_t, uint8_t d)
+{
+  data.push_back(d);
+}
+
 int TestMidi()
 {
-  std::vector<uint8_t> data;
   TMidi midi;
-  midi.SetUartSend([&data](uint32_t, uint8_t d) {
-      data.push_back(d);
-    });
+  midi.SetUartSend(push_data);
 
   midi.EnqueueByte(1);
   midi.EnqueueByte(2);
